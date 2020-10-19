66bc47b2c42c5761a1d2b97687812366a82cb5da
Konstantin Pelz <konstantin.pelz@online.de>
2020-10-20 00:23:59 +0200

    working on param tuning

--- parameterTuning.R
+++ parameterTuning.R
@@ -1,4 +1,12 @@
+library(subspace)
 source("C:\\Users\\Konstantin\\Desktop\\Uni\\6Semester\\BachelorArbeit\\code\\testdata.R")
+
+N <- 1000  # total number of rows to preallocate--possibly an overestimate
+
+result <- data.frame(name=rep("", N), dimNames=rep("", N),  # as many cols as you need
+                 diab1=rep(NA, N),diab2=rep(NA, N),diab3=rep(NA, N),diab4=rep(NA, N),
+                 diab5=rep(NA, N)) 
+
 
 small <- getData()
 smallMetab <- small[[1]]
@@ -7,8 +15,17 @@ T2D <- smallPheno["T2D"]
 #big <- getData(FALSE)
 #bigMetab <- big[[1]]
 #bigPheno <- big[[2]]
+counter<-1
+currentCombinationCounter <- 1
 
-cliqueClust <- CLIQUE(smallMetab, xi = 5,tau = 0.1)
+
+method <- "clique"
+xi <- 40
+tau <- 0.1
+
+combinations <- expand.grid(c(40,60,100), c(0.1,0.2,0.4))
+
+cliqueClust <- CLIQUE(smallMetab, xi = xi,tau = tau)
 cliqueClust <- cliqueClust[order(unlist(lapply(lapply(cliqueClust, "[[", "subspace"),"sum")),decreasing = TRUE)]
 nums <- unlist(lapply(lapply(cliqueClust, "[[", "subspace"),"sum"))
 top5 <- nums[1:5]
@@ -24,22 +41,9 @@ for (i in 1:5){
 
 realDiabs <- abs(0.5-top5Diab)
 
-if(!is.null(input$clusterId)){
-  if (input$clusterId!=0){
-    currentCluster <- clusteringData$Clique[as.numeric(input$clusterId)][[1]]
-    metabs <- names(metabComplete)[currentCluster$subspace]
-    usedMetabs <- sprintf("This cluster was calculed using the following metabolites: <br/>%s", paste(metabs, collapse = '<br/>'))
-    output$metabUsed <- renderUI(HTML(usedMetabs))
-    
-    clusterNumbers[currentCluster$objects] <- 17
-    finalValues$idsFromCluster <- currentCluster$objects
-  } else {
-    output$metabUsed <- renderUI(HTML("No cluster selected"))
-  }
-}
+result[counter, ] <- list(paste(method,xi,tau,sep = "_"),do.call("paste", c(top5Metabs, sep = "_")) ,top5Diab[[1]],top5Diab[[2]],top5Diab[[3]],top5Diab[[4]],top5Diab[[5]])
 
 
+result <- result[complete.cases(result),]
 
-cliqueMDSValues <- plotFromClusters(lapply(clusteringData$Clique, `[[`, "objects"), returnMDS = TRUE)
-clusteringData$CliqueMDS <- data.frame(cbind(seq_len(nrow(cliqueMDSValues)),cliqueMDSValues))
-names(clusteringData$CliqueMDS) <- c("id","x","y")
+write.table(result, file='output.tsv', quote = FALSE, row.names = FALSE, sep='\t')
