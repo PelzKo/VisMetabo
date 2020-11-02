87019f2a5cd8dea98fa6f941d3568df579c67958
Konstantin Pelz <konstantin.pelz@online.de>
2020-11-02 22:01:19 +0100

    rework parameterTuning for parallelisation

--- parameterTuning.R
+++ parameterTuning.R
@@ -22,7 +22,76 @@ calcW <- function(data,c=1){
   w
 }
 
+getExpressionTableForCluster <- function(cluster,pheno){
+  phenoClust <- length(cluster[pheno[cluster]==1])
+  phenoNonClust <- length(pheno[pheno==1])-phenoClust
+  nonPhenoClust <- length(cluster)-phenoClust
+  nonPhenoNonClust <- length(pheno[pheno==0])-nonPhenoClust
+  if (phenoClust+phenoNonClust+nonPhenoClust+nonPhenoNonClust!=length(pheno)){
+    print("ERROR, table is not correct")
+    return(NULL)
+  }
+  result <- data.frame(matrix(c(phenoClust,phenoNonClust,nonPhenoClust,nonPhenoNonClust),nrow = 2,ncol = 2))
+  names(result) <- c("pheno","nonPheno")
+  row.names(result) <- c("inCluster","outCluster")
+  result
+}
+
+calcValues <- function(idList,dimList,time){
+  significantClusters = numeric()
+  insignificantClusters = numeric()
+  pValues = numeric()
+  enrichmentScore = numeric()
+  
+  phenoAvg <- numeric()
+  phenoAvgSig <- numeric()
+  phenoAvgInSig <- numeric()
+  
+  for (i in seq_len(length(idList))){
+    expressionTableCluster <- getExpressionTableForCluster(idList[[i]],pheno)
+    chi <- chisq.test(expressionTableCluster)
+    pValues <- c(pValues,chi$p.value)
+    enrichmentScore <- c(enrichmentScore,chi$statistic)
+    phenoAvg <- c(phenoAvg,mean(pheno[idList[[i]]]))
+    if (chi$p.value<0.05){
+      significantClusters <- c(significantClusters,i)
+      phenoAvgSig <- c(phenoAvgSig,mean(pheno[idList[[i]]]))
+    } else {
+      insignificantClusters <- c(insignificantClusters,i)
+      phenoAvgInSig <- c(phenoAvgInSig,mean(pheno[idList[[i]]]))
+    }
+  }
+  
+  dims <- sapply(dimList,sum)
+  numInd <- sapply(idList,length)
+  
+  dimsSig <- dims[significantClusters]
+  numIndSig <- numInd[significantClusters]
+  
+  dimsInSig <- dims[-significantClusters]
+  numIndInSig <- numInd[-significantClusters]
+  
+  phenoAvg <- abs(mean-phenoAvg)
+  phenoAvgSig <- abs(mean-phenoAvgSig)
+  phenoAvgInSig <- abs(mean-phenoAvgInSig)
+  
+  idMaxEnrichment <- seq_len(length(enrichmentScore))[enrichmentScore == max(enrichmentScore)][[1]]
+  
+  maxEnrichment <- paste(enrichmentScore[[idMaxEnrichment]],pValues[[idMaxEnrichment]],sep = "_")
+  numberClusters <- paste(length(significantClusters),length(idList)-length(significantClusters),length(idList),sep ="_")
+  meanEnrichment <- paste(mean(enrichmentScore[significantClusters]),mean(enrichmentScore[-significantClusters]),mean(enrichmentScore),sep = "_")
+  meanPValue <- paste(mean(pValues[significantClusters]),mean(pValues[-significantClusters]),mean(pValues),sep = "_")
+  sumEnrichment <- paste(sum(enrichmentScore[significantClusters]),sum(enrichmentScore[-significantClusters]),sum(enrichmentScore),sep = "_")
+  sumPValue <- paste(sum(pValues[significantClusters]),sum(pValues[-significantClusters]),sum(pValues),sep = "_")
+  meanInd <- paste(mean(numIndSig),mean(numIndInSig),mean(numInd),sep = "_")
+  meanDims <- paste(mean(dimsSig),mean(dimsInSig),mean(dims),sep = "_")
+  meanPheno <- paste(mean(phenoAvgSig),mean(phenoAvgInSig),mean(phenoAvg),sep = "_")
+  
+  return(list(name,maxEnrichment,numberClusters,meanEnrichment,meanPValue,sumEnrichment,sumPValue,meanInd,meanDims,meanPheno,time))
+}
+
 args <- commandArgs(trailingOnly = TRUE)
+#args <- list("som","big","49","50","0.08,0.01","1Layer")
 print(args)
 
 method <- args[1]
@@ -32,11 +101,10 @@ N <- 1000  # total number of rows to preallocate--possibly an overestimate
 numberOfTimes <- 1:3 #number of times to repeat algorithms which differ each iteration
 counter <- 1 #current iteration
 
-result <- data.frame(name=rep(NA, N), dimNames=rep(NA, N),  # as many cols as you need
-                 diab1=rep(NA, N),diab2=rep(NA, N),diab3=rep(NA, N),diab4=rep(NA, N),
-                 diab5=rep(NA, N),numberOfIndividuals=rep(NA, N),meanDist=rep(NA, N)) 
+result <- data.frame(name=rep(NA, N), maxEnrichment=rep(NA, N),  # as many cols as you need
+                     numberClusters=rep(NA, N),meanEnrichment=rep(NA, N),meanPValue=rep(NA, N),sumEnrichment=rep(NA, N),
+                     sumPValue=rep(NA, N),meanInd=rep(NA, N),meanDims=rep(NA, N),meanPheno=rep(NA, N),time=rep(NA, N)) 
 logs <- list()
-
 
 if (counterSmallBig=="small"){
   small <- getData()
@@ -52,73 +120,66 @@ if (counterSmallBig=="small"){
 }
 mean <- mean(pheno)
 name <- ""
-ptm <- proc.time()
-
-args <- list("","","0.2","0.5","1")
 switch(method, 
        som={
-         run <- tryCatch({
-           gridSize <- as.numeric(args[3])
-           rlen <- as.numeric(args[4])
-           alphaSplit <- strsplit(as.character(args[5]),",")[[1]]
-           alpha <- c(as.numeric(alphaSplit[[1]]),as.numeric(alphaSplit[[2]]))
-           alphaAsString <- paste(alpha[[1]],alpha[[2]],sep = ";")
-           
-           name <- paste(method,counterSmallBig,gridSize,rlen,alphaAsString,sep = "_")
-
-           somClust <- som(data.matrix(data), grid = somgrid(xdim = sqrt(gridSize),ydim = sqrt(gridSize), topo = "hexagonal"),rlen = rlen, alpha = alpha)
-           somClustTwoLayers <- xyf(data.matrix(data),data.matrix(data), grid = somgrid(xdim = sqrt(gridSize),ydim = sqrt(gridSize), topo = "hexagonal"),rlen = rlen, alpha = alpha)
-           
-           clusterCounts <- sort(table(somClust$unit.classif),decreasing = TRUE)
-           clusterCounts <- clusterCounts[clusterCounts>4]
-           clusterCountsTwoLayers <- sort(table(somClustTwoLayers$unit.classif),decreasing = TRUE)
-           clusterCountsTwoLayers <- clusterCountsTwoLayers[clusterCountsTwoLayers>4]
-           
-           nums <- clusterCounts[1:5]
-           nums[is.na(nums)]<-0
-           numsTwoLayers <- clusterCountsTwoLayers[1:5]
-           numsTwoLayers[is.na(numsTwoLayers)]<-0
-           
-           top5 <- nums[1:5]
-           top5TwoLayers <- numsTwoLayers[1:5]
-           
-           top5Metabs <- list()
-           top5MetabsTwoLayers <- list()
-           
-           top5Diab <- rep(0.5,5)
-           top5DiabTwoLayers <- rep(0.5,5)
-           
-           ids <- 1:nrow(data)
-           minicount<-1
-           for (i in names(clusterCounts)[1:5]){
-             currentCluster <- ids[somClust$unit.classif==as.numeric(i)]
-             top5Diab[minicount] <- mean(pheno[currentCluster])
-             minicount<-minicount+1
+         for (round in numberOfTimes){
+           ptm <- proc.time()
+           run <- tryCatch({
+             gridSize <- as.numeric(args[3])
+             rlen <- as.numeric(args[4])
+             alphaSplit <- strsplit(as.character(args[5]),",")[[1]]
+             alpha <- c(as.numeric(alphaSplit[[1]]),as.numeric(alphaSplit[[2]]))
+             alphaAsString <- paste(alpha[[1]],alpha[[2]],sep = ";")
+             oneLayer <- args[6]=="1Layer"
+             
+             name <- paste(method,counterSmallBig,gridSize,rlen,alphaAsString,sep = "_")
+             
+             if (oneLayer){
+               somClust <- som(data.matrix(data), grid = somgrid(xdim = sqrt(gridSize),ydim = sqrt(gridSize), topo = "hexagonal"),rlen = rlen, alpha = alpha)
+             } else {
+               somClust <- xyf(data.matrix(data),data.matrix(data), grid = somgrid(xdim = sqrt(gridSize),ydim = sqrt(gridSize), topo = "hexagonal"),rlen = rlen, alpha = alpha)
+             }
+             timeRes <- proc.time() - ptm
+             
+             ids <- 1:nrow(data)
+             clusters <- list()
+             clusterCounts <- sort(table(somClust$unit.classif),decreasing = TRUE)
+             if (counterSmallBig=="small"){
+               clusterCounts <- clusterCounts[clusterCounts>4]
+             } else{
+               clusterCounts <- clusterCounts[clusterCounts>9]
+             }
+             
+             minicount<-1
+             for (i in names(clusterCounts)){
+               clusters[[minicount]] <- ids[somClust$unit.classif==as.numeric(i)]
+               minicount<-minicount+1
+             }
+             
+             idList <- clusters
+             dimList <- list()
+             
+             if (length(idList)==0){
+               result[counter, ] <- list(name,NA,NA,NA,NA,NA,NA,NA,NA,NA)
+               counter <- counter + 1
+               return(NA)
+             }
+             
+             result[counter, ] <- calcValues(idList,dimList,timeRes[[3]])
+             counter <- counter + 1
+             return(NA)
+           },
+           error = function(cond){
+             log <- c(paste(method,counterSmallBig,gridSize,rlen,alphaAsString,sep = "_"),cond$message,toString(cond$call))
+             return(log)
+           })
+           if (run[[2]]!="keine Funktion abzubrechen, springe zum Top Level"&run[[2]]!="no function to return from, jumping to top level"){
+             logs <- c(logs,run)
            }
-           top5Diab[is.na(top5Diab)]<-0.5
-           minicount<-1
-           for (i in names(clusterCountsTwoLayers)[1:5]){
-             currentCluster <- ids[somClustTwoLayers$unit.classif==as.numeric(i)]
-             top5DiabTwoLayers[minicount] <- mean(pheno[currentCluster])
-             minicount<-minicount+1
-           }
-           top5DiabTwoLayers[is.na(top5DiabTwoLayers)]<-0.5
-           
-           result[counter, ] <- list(paste(method,counterSmallBig,"1Layer",gridSize,rlen,alphaAsString,sep = "_"),"NO INFO" ,top5Diab[[1]],top5Diab[[2]],top5Diab[[3]],top5Diab[[4]],top5Diab[[5]],paste0(nums,collapse = "_"),mean(abs(mean-top5Diab)))
-           counter <- counter + 1
-           result[counter, ] <- list(paste(method,counterSmallBig,"2Layer",gridSize,rlen,alphaAsString,sep = "_"),"NO INFO" ,top5DiabTwoLayers[[1]],top5DiabTwoLayers[[2]],top5DiabTwoLayers[[3]],top5DiabTwoLayers[[4]],top5DiabTwoLayers[[5]],paste0(numsTwoLayers,collapse = "_"),mean(abs(mean-top5DiabTwoLayers)))
-           counter <- counter + 1
-           return(NA)
-         },
-         error = function(cond){
-           log <- c(paste(method,counterSmallBig,gridSize,rlen,alphaAsString,sep = "_"),cond$message,toString(cond$call))
-           return(log)
-         })
-         if (run[[2]]!="keine Funktion abzubrechen, springe zum Top Level"&run[[2]]!="no function to return from, jumping to top level"){
-           logs <- c(logs,run)
          }
        },
        cosa={
+         ptm <- proc.time()
          run <- tryCatch({
            lambda <- as.numeric(args[3])
            niter <- as.numeric(args[4])
@@ -127,25 +188,27 @@ switch(method,
            name <- paste(method,counterSmallBig,lambda,niter,noit,sep = "_")
          
            cosaClust <- cosa2(data, lambda=lambda, niter = niter, noit = noit)
+           timeRes <- proc.time() - ptm
+           
            hist <- hierclust(cosaClust$D, denplot = FALSE)
            clusters <- lapply(cut(hist$dendro, h=1.25)$lower,labels)
            clusters <- clusters[order(sapply(clusters,length),decreasing=T)]
-           clusters <- clusters[sapply(clusters, function(x) length(x)>4)]
-           
-           nums <- sapply(clusters,length)[1:5]
-           nums[is.na(nums)]<-0
-           
-           top5Diab <- rep(0.5,5)
-           
-           for (i in 1:5){
-             if (i<=length(clusters)){
-               currentCluster <- clusters[[i]]
-               top5Diab[i] <- mean(pheno[currentCluster])
-             }
+           if (counterSmallBig=="small"){
+             clusters <- clusters[sapply(clusters, function(x) length(x)>4)]
+           } else{
+             clusters <- clusters[sapply(clusters, function(x) length(x)>9)]
            }
-           top5Diab[is.na(top5Diab)]<-0.5
            
-           result[counter, ] <- list(paste(method,counterSmallBig,lambda,niter,noit,sep = "_"),"NO INFO" ,top5Diab[[1]],top5Diab[[2]],top5Diab[[3]],top5Diab[[4]],top5Diab[[5]],paste0(nums,collapse = "_"),mean(abs(mean-top5Diab)))
+           idList <- clusters
+           dimList <- list()
+           
+           if (length(idList)==0){
+             result[counter, ] <- list(name,NA,NA,NA,NA,NA,NA,NA,NA,NA)
+             counter <- counter + 1
+             return(NA)
+           }
+           
+           result[counter, ] <- calcValues(idList,dimList,timeRes[[3]])
            counter <- counter + 1
            return(NA)
            
@@ -161,6 +224,7 @@ switch(method,
        },
        doc={
          for (round in numberOfTimes){
+             ptm <- proc.time()
              run <- tryCatch({
                alpha <- as.numeric(args[3])
                beta <- as.numeric(args[4])
@@ -169,45 +233,33 @@ switch(method,
                name <- paste(method,counterSmallBig,alpha,beta,w,sep = "_")
              
                docClust <- runDoc(data, alpha, beta, w)
-               dimNums <- getDimsDoc(docClust)
-               #dims <- lapply(dimNums,function(x) names(data)[x])
-               dimNums <- dimNums[order(unlist(lapply(dimNums,"length")),decreasing = TRUE)]
+               timeRes <- proc.time() - ptm
                
-               dims <- unlist(lapply(dimNums,"length"))[1:5]
-               dims[is.na(dims)]<-0
+               dimNums <- getDimsDoc(docClust)
+               dimNums <- dimNums[order(unlist(lapply(dimNums,"length")),decreasing = TRUE)]
                
                idsInClustersDoc <- getIdsDoc(docClust)
                idsInClustersDoc <- idsInClustersDoc[order(unlist(lapply(dimNums,"length")),decreasing = TRUE)]
                
-               numInd <- unlist(lapply(idsInClustersDoc,length))[1:5]
-               numInd[is.na(numInd)]<-0
-               
                dimCondition <- sapply(dimNums, function(x) return(sum(x)>1))
                idsCondition <- sapply(idsInClustersDoc, function(x) return(length(x)>4))
+               if (counterSmallBig=="small"){
+                 idsCondition <- sapply(idsInClustersDoc, function(x) return(length(x)>4))
+               } else{
+                 idsCondition <- sapply(idsInClustersDoc, function(x) return(length(x)>9))
+               }
                condition <- dimCondition&idsCondition
                
-               dimNums <- dimNums[condition]
-               idsInClustersDoc <- idsInClustersDoc[condition]
+               idList <- idsInClustersDoc[condition]
+               dimList <- dimNums[condition]
                
-               dimsAndIndividuals <- paste0(c(dims,numInd),collapse="_")
-               
-               top5Metabs <- list()
-               top5Diab <- rep(0.5,5)
-               
-               
-               for (i in 1:5){
-                 if (i<=length(idsInClustersDoc)&i<=length(dimNums)){
-                   currentCluster <- idsInClustersDoc[[as.numeric(i)]]
-                   top5Metabs[[i]] <- paste0(names(data)[dimNums[[as.numeric(i)]]], collapse = "_")
-                   
-                   top5Diab[i] <- mean(pheno[currentCluster])
-                 }
+               if (length(idList)==0){
+                 result[counter, ] <- list(name,NA,NA,NA,NA,NA,NA,NA,NA,NA)
+                 counter <- counter + 1
+                 return(NA)
                }
-               top5Diab[is.na(top5Diab)]<-0.5
                
-               #realDiabs <- abs(0.5-top5Diab)
-               
-               result[counter, ] <- list(paste(method,counterSmallBig,alpha,beta,w,sep = "_"),paste0(top5Metabs,collapse = ";") ,top5Diab[[1]],top5Diab[[2]],top5Diab[[3]],top5Diab[[4]],top5Diab[[5]],dimsAndIndividuals,mean(abs(mean-top5Diab)))
+               result[counter, ] <- calcValues(idList,dimList,timeRes[[3]])
                counter <- counter + 1
                return(NA)
              },
@@ -222,6 +274,7 @@ switch(method,
        },
        {
          # default is using Clique
+         ptm <- proc.time()
          run <- tryCatch({
            xi <- as.numeric(args[3])
            tau <- as.numeric(args[4])
@@ -229,32 +282,25 @@ switch(method,
            name <- paste(method,counterSmallBig,xi,tau,sep = "_")
          
            cliqueClust <- CLIQUE(data, xi = xi,tau = tau)
+           timeRes <- proc.time() - ptm
+           
            cliqueClust <- cliqueClust[order(unlist(lapply(lapply(cliqueClust, "[[", "subspace"),"sum")),decreasing = TRUE)]
-           cliqueClust <- cliqueClust[sapply(cliqueClust, function(x) return(sum(x$subspace)>1&length(x$objects)>4))]
-           
-           
-           dims <- unlist(lapply(lapply(cliqueClust, "[[", "subspace"),"sum"))[1:5]
-           dims[is.na(dims)]<-0
-           numInd <- unlist(lapply(lapply(cliqueClust, "[[", "objects"),"length"))[1:5]
-           numInd[is.na(numInd)]<-0
-           
-           dimsAndIndividuals <- paste0(c(dims,numInd),collapse="_")
-
-           top5Metabs <- list()
-           top5Diab <- rep(0.5,5)
-           
-           
-           for (i in 1:5){
-             currentCluster <- cliqueClust[as.numeric(i)][[1]]
-             top5Metabs[[i]] <- paste0(names(data)[currentCluster$subspace], collapse = "_")
-             
-             mean(pheno[currentCluster$objects])
+           if (counterSmallBig=="small"){
+             cliqueClust <- cliqueClust[sapply(cliqueClust, function(x) return(sum(x$subspace)>1&length(x$objects)>4))]
+           } else{
+             cliqueClust <- cliqueClust[sapply(cliqueClust, function(x) return(sum(x$subspace)>1&length(x$objects)>9))]
            }
-           top5Diab[is.na(top5Diab)]<-0.5
            
-           #realDiabs <- abs(0.5-top5Diab)
+           idList <- lapply(cliqueClust, "[[", "objects")
+           dimList <- lapply(cliqueClust, "[[", "subspace")
            
-           result[counter, ] <- list(paste(method,counterSmallBig,xi,tau,sep = "_"),paste0(top5Metabs,collapse = ";") ,top5Diab[[1]],top5Diab[[2]],top5Diab[[3]],top5Diab[[4]],top5Diab[[5]],dimsAndIndividuals,mean(abs(mean-top5Diab)))
+           if (length(idList)==0){
+             result[counter, ] <- list(name,NA,NA,NA,NA,NA,NA,NA,NA,NA)
+             counter <- counter + 1
+             return(NA)
+           }
+           
+           result[counter, ] <- calcValues(idList,dimList,timeRes[[3]])
            counter <- counter + 1
            return(NA)
          },
@@ -268,14 +314,9 @@ switch(method,
     }
 )
 
-timeRes <- proc.time() - ptm
-
-
-
 result <- result[complete.cases(result[[1]]),]
-result$time <- timeRes[[3]]
-write.table(result, file=paste0(name,'_output.tsv'), quote = FALSE, row.names = FALSE, sep='\t')
+write.table(result, file=paste0("results/",name,'_output.tsv'), quote = FALSE, row.names = FALSE, sep='\t')
 if (length(logs)!=0){
   logsToWrite <- data.frame(matrix(unlist(logs), nrow=length(unlist(logs))/3, byrow=T))
-  write.table(logsToWrite, file=paste0(name,'_logs.tsv'), quote = FALSE, row.names = FALSE, col.names = FALSE, sep='\t')
+  write.table(logsToWrite, file=paste0("results/",name,'_logs.tsv'), quote = FALSE, row.names = FALSE, col.names = FALSE, sep='\t')
 }
