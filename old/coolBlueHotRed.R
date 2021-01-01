aa23023d9cc68bb839911c819e67ba93cc55515b
Konstantin Pelz <konstantin.pelz@online.de>
2021-01-02 00:29:46 +0100

    cleanup

--- old/coolBlueHotRed.R
+++
@@ -1,9 +0,0 @@
-# Palette defined by kohonen package
-#
-# Shane Lynn 14-01-2014
-
-coolBlueHotRed <- function(n, alpha = 1) {
-  #rainbow(n, end=4/6, alpha=alpha)[n:1]
-  #colorRampPalette(c("red","white","blue"), space="Lab")(20)
-  colorRampPalette(c("red","white","blue"), space="Lab")(n)
-}
--- recursionTest.R
+++
@@ -1,7 +0,0 @@
-library(rlist)
-getClusters <- function(dendro,limit=10){
-  if (nobs(dendro)<limit){
-    return(NULL)
-  }
-  return(list(unlist(dendro),getClusters(dendro[[1]],limit),getClusters(dendro[[2]],limit)))
-}
